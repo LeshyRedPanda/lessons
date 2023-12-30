d95bdb161e3bae339b2a4c4b82e521b59aa5c37c
leshy <sokolov1993@gmail.com>
2023-12-30 16:10:22 +0200

    014

--- mainJs.js
+++ mainJs.js
@@ -1,7 +1,7 @@
 "use strict";
 
 
-//014
+
 //debugger
 
 
@@ -15,101 +15,7 @@ const personalMovieDB = {
     privat: false
 };
 
-/*const a = prompt ("what last movie did u see?",""),
-      b = prompt ("How would you rate it?",""),
-      c = prompt ("what last movie did u see?",""),
-      d = prompt ("How would you rate it?","");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
-console.log(personalMovieDB);*/
-
-//015
-
-/*1
-for (let i = 0; i < 2; i++ ){
-    const a = prompt ("what last movie did u see?",""),
-          b = prompt ("How would you rate it?","");
-          personalMovieDB.movies[a] = b;
-}
-
-console.log(personalMovieDB);*/
-/*2
-for (i = 0; i < 2; i++){
-    const a = prompt ("what last movie did u see?",""),
-          b = prompt ("How would you rate it?","");
-          personalMovieDB.movies[a] = b;
-}
-
-console.log(personalMovieDB);*/
-
-/*3
-for (let i = 0; i < 2; i++ ){
-    const a = prompt ("what last movie did u see?",""),
-          b = prompt ("How would you rate it?","");
-          personalMovieDB.movies[a] = b;
-}
-
-console.log(personalMovieDB);*/
-/*4
-for (let i = 0; i < 2; i++) {
-    const a = prompt("what last movie did u see?", ""),
-          b = prompt("How would you rate it?", "");
-
-    if (a != null && b != null && a != "" && b != "" && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log("done");
-    } else {
-        console.log("error");
-        i--
-    }
-}
-
-if (personalMovieDB.count < 10) {
-    alert("you don't watch enough movies");
-} else if  (personalMovieDB.count > 10 && personalMovieDB.count < 30){
-    alert("good boy");
-}else if (personalMovieDB.count > 30){
-    alert("you spend too much time on movies");
-    
-} else {
-    alert("error")
-}
-console.log(personalMovieDB);*/
-
-/*5
-//debugger
-
-for (let i = 0; i < 2; i++) {
-    const a = prompt("what last movie did u see?", ""),
-        b = prompt("How would you rate it?", "");
-
-
-    if (a != null && b != null && a != "" && b != "" && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('ok');
-    }
-    else {
-        console.log('error');
-        i--
-    }
-}
-
-if (personalMovieDB.count < 10) {
-    alert("you don't watch enough movies");
-} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
-    alert("good boy");
-} else if (personalMovieDB.count > 30) {
-    alert("you spend too much time on movies");
-    
-} else {
-    alert("error");
-    
-}
-
-console.log(personalMovieDB);
-*/
 
 for (let i = 0; i <2; i++ ){
    const a = prompt ("what last movie did u see?",""),
@@ -139,3 +45,5 @@ if (personalMovieDB.count < 10 ){
         alert("error");
     }
 console.log(personalMovieDB);
+
+
